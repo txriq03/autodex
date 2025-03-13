fbe210d13757b398328bb449ff16b30b6d550b2d
Tariq <118397532+txriq03@users.noreply.github.com>
2025-03-13 16:05:50 +0000

    fix: Fixed balance not showing correct value when connecting to wallet

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React, { useContext, useEffect, useState } from "react";
 import { Button } from "./ui/button";
 import { ContractContext } from "./providers/ContractProvider";
@@ -6,37 +6,42 @@ import { ethers } from "ethers";
 import Link from "next/link";
 
 const Navbar = () => {
-  const {account, setAccount, provider} = useContext(ContractContext);
+  const { account, setAccount, provider } = useContext(ContractContext);
   const [balance, setBalance] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchBalance = async () => {
-      if (account && provider) {
+      if (!account || !provider) return;
+      try {
         const rawBalance = await provider.getBalance(account);
-        setBalance(ethers.formatEther(rawBalance));
+        const formatted = ethers.formatEther(rawBalance);
+        setBalance(formatted);
+      } catch (err) {
+        console.error("Failed to fetch balance:", err);
       }
     };
     fetchBalance();
-  }, [account, provider])
+  }, [account, provider]);
 
   return (
     <div className=" bg-slate-800 py-3">
       <div className="min-[1440px]:max-w-[1440px] mx-5 min-[1450px]:mx-auto flex justify-between">
-        
         <Link href="/">
           <div className="text-2xl text-white font-semibold">AutoDex</div>
         </Link>
 
         {account ? (
           <div className="text-2xl text-white ">
-            <p className="inline font-bold text-teal-400">{Number(balance).toFixed(2)}</p>  <p className="inline">ETH</p>
+            <p className="inline font-bold text-teal-400">
+              {Number(balance).toFixed(2)}
+            </p>{" "}
+            <p className="inline">ETH</p>
           </div>
         ) : (
           <div>
             <Button variant="secondary">Connect Wallet</Button>
           </div>
         )}
-
       </div>
     </div>
   );
---
+++ client/components/VehicleGrid.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const VehicleGrid = () => {
+  return (
+    <div>VehicleGrid</div>
+  )
+}
+
+export default VehicleGrid
