ef9fea2e2030f46e342f6ea3b6b19aac618ca403
Tariq <118397532+txriq03@users.noreply.github.com>
2025-03-13 21:41:16 +0000

    chore: Made a separate component for vehicle cards

--- client/components/VehicleGrid.tsx
+++ client/components/VehicleGrid.tsx
@@ -17,23 +17,29 @@ const VehicleGrid = () => {
         <h1 className="text-[2rem] ">Vehicles minted</h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
             {cars?.map((car: any, index: number) => (
-                <div key={index} className="border p-4 rounded-xl shadow">
-                    <Image src={car.image} alt={car.model} height={400} width={400} className="w-full h-48 object-cover rounded-md mb-2"/>
-                    <h2 className="text-xl font-bold">{car.make.charAt(0).toUpperCase() + car.make.slice(1)} {car.model.charAt(0).toUpperCase() + car.model.slice(1)}</h2>
-                    <p><strong>Year:</strong> {car.year}</p>
-                    <p><strong>Mileage:</strong> {car.mileage} km</p>
-                    <p><strong>VIN:</strong> {car.vin}</p>
-                    <div><strong>Status:</strong> <p className={`inline ${car.forSale ? 'text-teal-500' : 'text-rose-500'}`}>{car.forSale ? "Available" : "Sold"}</p></div>
-                    <div className="flex justify-between mt-4 items-end">
-                        <Button>Purchase</Button>
-                        <div className="text-[1.5rem] font-bold">{car.price} <p className="text-slate-400 inline">ETH</p></div>
-                    </div>
-                </div>
+                <VehicleCard key={index} car={car}  />
             ))}
         </div>
     </div>
   );
 };
+
+const VehicleCard = ({ car, index}: any) => {
+    return (
+        <div className="border p-4 rounded-xl shadow">
+            <Image src={car.image} alt={car.model} height={400} width={400} className="w-full h-48 object-cover rounded-md mb-2"/>
+            <h2 className="text-xl font-bold">{car.make.charAt(0).toUpperCase() + car.make.slice(1)} {car.model.charAt(0).toUpperCase() + car.model.slice(1)}</h2>
+            <p><strong>Year:</strong> {car.year}</p>
+            <p><strong>Mileage:</strong> {car.mileage} km</p>
+            <p><strong>VIN:</strong> {car.vin}</p>
+            <div><strong>Status:</strong> <p className={`inline ${car.forSale ? 'text-teal-500' : 'text-rose-500'}`}>{car.forSale ? "Available" : "Sold"}</p></div>
+            <div className="flex justify-between mt-4 items-end">
+                <Button>Purchase</Button>
+                <div className="text-[1.5rem] font-bold">{car.price} <p className="text-slate-400 inline">ETH</p></div>
+            </div>
+        </div>
+    )
+}
 
 export const useAllCars = () => {
   const { contract } = useContext(ContractContext); // or pass contract in manually
