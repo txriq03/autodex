ee55dfb4e4670c6bd473d732ed40821982b23314
Tariq <118397532+txriq03@users.noreply.github.com>
2025-03-14 19:07:18 +0000

    refactor: change solidity code to reduce gas fees by using tokenURI

--- contract/contracts/CarMarketplace.sol
+++ contract/contracts/CarMarketplace.sol
@@ -9,18 +9,13 @@ contract CarMarketplace is ERC721URIStorage, Ownable {
     uint256 private nextTokenId;
 
     struct Car {
-        string make;
-        string model;
-        uint256 year;
-        string vin; // Unique Vehicle Identification Number
-        uint256 mileage;
-        bool forSale;
-        uint256 price;
+        uint256 tokenId;
         string tokenURI;
+        uint256 price; 
+        address payable owner;
     }
 
     mapping(uint256 => Car) private cars; // tokenID mapped to Car struct
-    mapping(uint256 => address payable) private carOwners; // tokenId linked to owner's address
     mapping(string => uint256) private vinToId; 
 
     event CarMinted(uint256 tokenId, address owner, string vin);
@@ -32,31 +27,29 @@ contract CarMarketplace is ERC721URIStorage, Ownable {
     // Function to mint a new car token
     function mintCar(
         address to,
-        string memory make,
-        string memory model,
-        uint256 year,
         string memory vin,
-        uint256 mileage,
         uint256 price,
         string memory tokenURI
     ) public onlyOwner {
         require(bytes(vin).length > 0, "VIN must be provided");
 
         // Check if the VIN already exists, while getting the correct nextTokenId
-        for (uint256 i = 0; i < nextTokenId; i++) {
-            require(
-                keccak256(abi.encodePacked(cars[i].vin)) !=
-                    keccak256(abi.encodePacked(vin)),
-                "VIN already exists"
-            );
-        }
+        require(vinToId[vin] == 0 && nextTokenId != 0, "VIN already used");
 
         uint256 tokenId = nextTokenId;
-        cars[tokenId] = Car(make, model, year, vin, mileage, true, price, tokenURI);
-        carOwners[tokenId] = payable(to);
-        vinToId[vin] = tokenId;
+
         _mint(to, tokenId);
         _setTokenURI(tokenId, tokenURI);
+
+        // Creating the car struct
+        cars[tokenId] = Car({
+            tokenId: tokenId,
+            tokenURI: tokenURI,
+            owner: payable(to),
+            price: price
+        });
+
+        vinToId[vin] = tokenId;
         nextTokenId++;
         emit CarMinted(tokenId, to, vin);
     }
@@ -69,17 +62,18 @@ contract CarMarketplace is ERC721URIStorage, Ownable {
         );
         require(price > 0, "Price must be greater than zero.");
 
-        cars[tokenId].forSale = true;
         cars[tokenId].price = price;
         emit CarListedForSale(tokenId, price);
     }
 
     function buyCar(uint256 tokenId) public payable {
-        require(cars[tokenId].forSale, "Car is not for sale");
+        Car storage car = cars[tokenId];
+        address payable seller = car.owner;
+        
+        require(cars[tokenId].price > 0, "Car is not for sale");
         require(msg.value >= cars[tokenId].price, "Insufficient ETH sent.");
-
-        address payable seller = carOwners[tokenId];
         require(seller != msg.sender, "You cannot buy your own car.");
+
 
         // Transfer ownership
         _transfer(seller, msg.sender, tokenId);
@@ -88,12 +82,11 @@ contract CarMarketplace is ERC721URIStorage, Ownable {
         seller.transfer(msg.value);
 
         // Update ownership details and reset values
-        carOwners[tokenId] = payable(msg.sender);
-        cars[tokenId].forSale = false;
+        car.owner = payable(msg.sender);
         cars[tokenId].price = 0;
 
         emit CarSold(tokenId, msg.sender, msg.value);
-    }
+    }   
 
     // Function to get car details by tokenId
     function getCarDetails(uint256 tokenId) public view returns (Car memory) {
@@ -112,9 +105,9 @@ contract CarMarketplace is ERC721URIStorage, Ownable {
         return all;
     }
 
-    function getTokenIdByVIN(string memory vin) public view returns (uint256) {
-        uint256 tokenId = vinToId[vin];
-        require(bytes(cars[tokenId].vin).length > 0, "VIN not found");
-        return tokenId;
-    }
+    // function getTokenIdByVIN(string memory vin) public view returns (uint256) {
+    //     uint256 tokenId = vinToId[vin];
+    //     require(bytes(cars[tokenId].vin).length > 0, "VIN not found");
+    //     return tokenId;
+    // }
 }
