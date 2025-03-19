0f1b012e891a74832573a87351883e5c19b7e6f3
Tariq <118397532+txriq03@users.noreply.github.com>
2025-03-19 23:40:13 +0000

    feat: added car information path

--- client/components/VehicleCardMenu.tsx
+++ client/components/VehicleCardMenu.tsx
@@ -1,11 +1,16 @@
-"use client"
+"use client";
 import React from "react";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
 import { Button } from "./ui/button";
-import { Ellipsis, Hammer, ScrollText} from "lucide-react";
+import { Book, Ellipsis, Hammer, ScrollText } from "lucide-react";
 import Link from "next/link";
 
-const VehicleCardMenu = ({vin}: {vin: string}) => {
+const VehicleCardMenu = ({ vin }: { vin: string }) => {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -18,16 +23,21 @@ const VehicleCardMenu = ({vin}: {vin: string}) => {
         align="end"
         className="w-fit rounded-md shadow-md bg-white border p-1"
       >
-          <Link href={`/logs/${vin}`}>
-        <DropdownMenuItem className="text-sm hover:bg-slate-100 rounded px-2 py-1.5 cursor-pointer">
-            Service logs <ScrollText className="text-slate-600"/>
-        </DropdownMenuItem>
-          </Link>
-        <DropdownMenuItem disabled className="text-sm text-slate-500 rounded px-2 py-1.5 ">
-            Info <Hammer />
-        </DropdownMenuItem>
-        <DropdownMenuItem disabled className="text-sm text-slate-500 rounded px-2 py-1.5 ">
-            Report <Hammer />
+        <Link href={`/cars/${vin}`}>
+          <DropdownMenuItem className="text-sm hover:bg-slate-100 rounded px-2 py-1.5 cursor-pointer flex justify-between">
+            Information <Book className="text-slate-600" />
+          </DropdownMenuItem>
+        </Link>
+        <Link href={`/logs/${vin}`}>
+          <DropdownMenuItem className="text-sm hover:bg-slate-100 rounded px-2 py-1.5 cursor-pointer flex justify-between">
+            Service logs <ScrollText className="text-slate-600" />
+          </DropdownMenuItem>
+        </Link>
+        <DropdownMenuItem
+          disabled
+          className="text-sm text-slate-500 rounded px-2 py-1.5 "
+        >
+          Delete <Hammer />
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
