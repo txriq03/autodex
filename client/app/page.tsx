9d4b6a87a06e1a23dc5907df8a5020fdafc1470d
Tariq <118397532+txriq03@users.noreply.github.com>
2025-04-01 23:59:56 +0100

    fix: fixed vercel deployment

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,14 +1,25 @@
+"use client";
 import Hero from "@/components/Hero";
 import VehicleContent from "@/components/VehicleContent";
+import { Suspense } from "react";
+import { Spinner } from "@heroui/spinner";
 
 export default function Home() {
   return (
-    <div className=" ">
+    <Suspense>
       <Hero />
       <div className="min-[1440px]:max-w-[1440px] mx-5 min-[1450px]:mx-auto text-slate-50">
-        <h1 className="text-[2rem]  font-bold">Marketplace</h1>
+        <h1 className="text-[2rem] font-bold">Marketplace</h1>
         <VehicleContent />
       </div>
-    </div>
+    </Suspense>
   );
 }
+
+const loadingBox = () => {
+  return (
+    <div className="rounded-lg">
+      <Spinner variant="simple" />
+    </div>
+  );
+};
