c9668d4f1f57b76091cb0625f2388ffa1d3a0dbb
Tariq <118397532+txriq03@users.noreply.github.com>
2025-04-12 22:26:44 +0100

    refactor: deployed updated contract and updated relevant contract variables

--- client/lib/constants.ts
+++ client/lib/constants.ts
@@ -1,4 +1,5 @@
 import abi from "./web3/CarMarketplace.json";
 
-export const CONTRACT_ADDRESS = "0x94eeeac81e8C6E3D78475cD05984D757B1b8b8a8";
+// export const CONTRACT_ADDRESS = "0x94eeeac81e8C6E3D78475cD05984D757B1b8b8a8";
+export const CONTRACT_ADDRESS = "0x12ab76552Fc773E1Ebaa50f1EB39bd122409E16D";
 export const CONTRACT_ABI = abi.abi;
